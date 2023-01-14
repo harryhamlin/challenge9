ffc458173d60662e6623bb553eab3d4b44c4b1e1
bulk <harryhamlin@gmail.com>
2023-01-13 16:24:10 -0800

    added .gitignore and some sample code

---
+++ Develop/index.js
@@ -0,0 +1,52 @@
+// TODO: Include packages needed for this application
+
+
+
+// sections:
+// title
+// tableofcontents (clickeable hyperlinks)
+// license (input should be checkboxes)
+// installation
+// usage
+// contributing (input should include github username/output: link to the github page)
+// tests 
+// questions (input should include email address)
+
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
+// TODO: Create an array of questions for user input
+// const questions = [];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
